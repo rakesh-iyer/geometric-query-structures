2db3286d10c6ff860b1f85699baa2590461afad5
Rakesh Iyer <rakeiyer@amazon.com>
2018-04-07 08:54:37 -0700

    Generalize the Single Range Tree

--- SingleRangeTree.java
+++ SingleRangeTree.java
@@ -1,27 +1,39 @@
 import java.util.*;
 
+//only supports integer keys at this moment.
 class SingleRangeTree {
     Node n;
+
+    static class KeyValue {
+        int key;
+        Object value;
+
+        KeyValue(int key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     static class Node {
         Node left;
         Node right;
-        Integer key;
+        KeyValue keyValue;
         boolean isLeaf() {
             return left == null && right == null;
         }
     }
 
-    SingleRangeTree(int arr[]) {
+    SingleRangeTree(KeyValue arr[]) {
         n = construct(arr, 0, arr.length-1);
     }
 
-    Node construct(int arr[], int start, int end) {
+    Node construct(KeyValue arr[], int start, int end) {
         Node node = new Node();
 
         // input is a sorted array, create a balanced tree with each array element as leaf.
         int mid = (start + end)/2;
 
-        node.key = arr[mid];
+        node.keyValue = arr[mid];
 
         if (start != end) {
             node.left = construct(arr, start, mid);
@@ -31,8 +43,8 @@ class SingleRangeTree {
         return node;
     }
 
-    List<Integer> subtree(Node n) {
-        List<Integer> l = new ArrayList<>();
+    List<KeyValue> subtree(Node n) {
+        List<KeyValue> l = new ArrayList<>();
 
         subtree(n, l);
 
@@ -40,24 +52,24 @@ class SingleRangeTree {
     }
 
 
-    void subtree(Node n, List<Integer> l) {
+    void subtree(Node n, List<KeyValue> l) {
         if (n == null) {
             return;
         }
         subtree(n.left, l);
         if (n.isLeaf()) {
-            l.add(n.key);
+            l.add(n.keyValue);
         }
 
         subtree(n.right, l);
     }
 
-    List<Integer> query(int start, int end) {
-        List<Integer> list = new ArrayList<>();
+    List<KeyValue> query(int start, int end) {
+        List<KeyValue> list = new ArrayList<>();
 
         // traverse both start and end until, they split.
-        while (!(n.isLeaf() || (n.key >= start && n.key < end))) {
-            if (n.key >= start) {
+        while (!(n.isLeaf() || (n.keyValue.key >= start && n.keyValue.key < end))) {
+            if (n.keyValue.key >= start) {
                 n = n.left;
             } else {
                 n = n.right;
@@ -65,8 +77,8 @@ class SingleRangeTree {
         }
 
         if (n.isLeaf()) {
-            if (n.key >= start && n.key <= end) {
-                list.add(n.key);
+            if (n.keyValue.key >= start && n.keyValue.key <= end) {
+                list.add(n.keyValue);
             }
             return list;
         }
@@ -76,7 +88,7 @@ class SingleRangeTree {
 
         // go trace to start and output right subtree whenever you gp left.
         while (!p.isLeaf()) {
-            if (p.key >= start) {
+            if (p.keyValue.key >= start) {
                 list.addAll(subtree(p.right));
                 p = p.left;
             } else {
@@ -84,14 +96,14 @@ class SingleRangeTree {
             }
         }
 
-        if (p.key >= start && p.key <= end) {
-            list.add(p.key);
+        if (p.keyValue.key >= start && p.keyValue.key <= end) {
+            list.add(p.keyValue);
         }
 
         p = n.right;
 
         while (!p.isLeaf()) {
-            if (p.key < end) {
+            if (p.keyValue.key < end) {
                 list.addAll(subtree(p.left));
                 p = p.right;
             } else {
@@ -99,22 +111,22 @@ class SingleRangeTree {
             }
         }
 
-        if (p.key >= start && p.key <= end) {
-            list.add(p.key);
+        if (p.keyValue.key >= start && p.keyValue.key <= end) {
+            list.add(p.keyValue);
         }
 
         return list;
     }
 
     public static void main(String args[]) {
-        int arr[] = {1, 100, 101, 115, 120, 220, 300};
+       KeyValue kv[] = {new KeyValue(1, null), new KeyValue(100, null), new KeyValue(101, null), new KeyValue(115, null),new KeyValue(120, null), new KeyValue(220, null), new KeyValue(300, null)};
 
-        SingleRangeTree srTree = new SingleRangeTree(arr);
+        SingleRangeTree srTree = new SingleRangeTree(kv);
 
-        List<Integer> l = srTree.query(100, 120);
+        List<KeyValue> l = srTree.query(100, 120);
 
-        for (int i : l) {
-            System.out.println(i);
+        for (KeyValue keyValue : l) {
+            System.out.println(keyValue.key);
         }
     }
 }
